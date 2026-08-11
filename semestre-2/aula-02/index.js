53f0af0ef4525759a680c38b8fd647a894178a39
itoikk <itoiandre14@gmail.com>
2026-08-11 15:06:19 -0300

    ta prontu

--- semestre-2/aula-02/index.js
+++ semestre-2/aula-02/index.js
@@ -1,4 +1,25 @@
-const _ = require('lodash');
+const axios = require('axios');
+const lodash = require('lodash');
 
-const texto = "Olá, Mundo!";
-console.log(_.capitalize(texto));
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Qual filme você quer? ",function(a){
+    axios.get(`https://api.tvmaze.com/search/shows?q=${a}`)
+    .then(response => {
+            const serie = response.data.find(item => item.show.name === a);
+            console.log("Título:", serie.show.name);
+            console.log("Gêneros:", lodash.join(serie.show.genres, ', '));
+            console.log("Resumo", lodash.trim(lodash.replace(serie.show.summary, /<[^>]*>/g, '')));
+            console.log("URL:", serie.show.officialSite || "Sem site!");
+
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+    rl.close();
+});
